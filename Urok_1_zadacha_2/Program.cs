d760ad3cb037f6f333909903c51cc8e30ee5bb12
Arteeex <arteeex187@gmail.com>
2023-01-16 11:46:03 +0700

    Urok_1_zadacha_2

---
+++ Urok_1_zadacha_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write(">>> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(">>> ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
