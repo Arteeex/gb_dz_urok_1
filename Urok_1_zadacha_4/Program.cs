804250ddd35cbbb380226cec7aeea307ac51813e
Arteeex <arteeex187@gmail.com>
2023-01-16 12:11:10 +0700

    Urok_1_zadacha_4

--- Urok_1_zadacha_4/Program.cs
+++ Urok_1_zadacha_4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write(">>> ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write(">>> ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write(">>> ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b && a > c)
+{
+    Console.WriteLine($"Максимальное из трёх: {a}");
+}
+else if (b > a && b > c)
+{
+    Console.WriteLine($"Максимальное из трёх: {b}");
+}
+else 
+{
+    Console.WriteLine($"Масимальное из трёх: {c}");
+}
