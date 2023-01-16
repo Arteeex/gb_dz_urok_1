1c798a55819005df689c97d82385c0c9eeeab369
Arteeex <arteeex187@gmail.com>
2023-01-16 12:17:24 +0700

    Urok_1_zadacha_6

--- Urok_1_zadacha_6/Program.cs
+++ Urok_1_zadacha_6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write(">>> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
